be14c5ddfaa9773125aa67e07da5ad6b14dd65e1
Annie <nsab42@yahoo.com>
2017-01-27 11:22:00 -0800

    Able to list all the members in the All members page and ready directly from the database

---
+++ src/app/member.model.ts
@@ -0,0 +1,3 @@
+export class Member {
+  constructor (public memberType: string, public name: string, public age: number, public description: string, public picture: string) { }
+}
--- src/app/members-repo/members-repo.component.ts
+++ src/app/members-repo/members-repo.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { Router } from '@angular/router';
+// import { Member } from './member.model';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { MemberService } from '../member.service';
 
 @Component({
   selector: 'app-members-repo',
   templateUrl: './members-repo.component.html',
-  styleUrls: ['./members-repo.component.css']
+  styleUrls: ['./members-repo.component.css'],
+  providers: [MemberService]
 })
 export class MembersRepoComponent implements OnInit {
+  members: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
+  // formShowing:boolean = false;
 
-  constructor() { }
+  constructor(private router: Router, private memberService: MemberService) {}
+
+  goToDetailPage(clickedMember) {
+    this.router.navigate(['members', clickedMember.$key]);
+  }
 
   ngOnInit() {
+    this.members = this.memberService.getMembers();
   }
+
+  // showForm(){
+  //   this.formShowing=true;
+  // }
 
 }
